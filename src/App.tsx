4e8e19d0943e72fa7e0e8d4d531ce75a4655e8a5
luislongo <luis.longof@gmail.com>
2023-10-18 20:48:40 -0300

    Adds minimal rep of the list

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Input } from "./tree/Tree.types";
+import { createTree } from "./tree/createTree";
+import { flattenTree } from "./tree/flattenTree";
+
+const inputMock: Input<Record<string, unknown>>[] = [
+  {
+    path: "fileA.ts",
+  },
+  {
+    path: "fileB.ts",
+  },
+  {
+    path: "folderA/fileC.ts",
+  },
+  {
+    path: "folderA/fileD.ts",
+  },
+  {
+    path: "folderB/fileE.ts",
+  },
+  {
+    path: "folderB/fileF.ts",
+  },
+];
+
+export const App = () => {
+  const rows = flattenTree(createTree(inputMock));
+  return (
+    <div>
+      {rows.map((row) => {
+        return <div key={row.path}>{row.path}</div>;
+      })}
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
+import { App } from "./App";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<div>AAA</div>);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
